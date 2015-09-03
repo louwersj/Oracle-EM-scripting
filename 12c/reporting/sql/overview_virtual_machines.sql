79440f4ad911916fed49ed292b7000203988e430
Johan Louwers <louwersj@gmail.com>
2015-09-03 16:17:30 +0200

    Update overview_virtual_machines.sql
    Added comment

--- 12c/reporting/sql/overview_virtual_machines.sql
+++ 12c/reporting/sql/overview_virtual_machines.sql
@@ -1,3 +1,34 @@
+/* NAME:
+ *   overview_virtual_machines.sql
+ *
+ * DESC:
+ *   Provides an overview of all VM's running on OVM from an OEM 
+ *   point of view. This provides a quick view into your landscape
+ *   and where VM's are located. 
+ * 
+ * LOG:
+ * VERSION---DATE--------NAME-------------COMMENT
+ * 0.1       03SEP2015   Johan Louwers    Initial upload to github.com
+ *
+ * LICENSE:
+ * Copyright (C) 2014  Johan Louwers
+ * 
+ * This code is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this code; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ */
+
 SELECT 
    v_ovm_vm.ovm_display_name         AS VM_NAME,
    v_ovm_vm.kernel_ver               AS VM_KERNEL,
